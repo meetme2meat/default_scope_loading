0c7804eed5c58ad24fe32b60800584ecf8ef13b2
Viren Negi <meetme2meat@users.noreply.github.com>
2018-04-23 08:07:15 +0530

    Updated the code.
    The issue has nothing to do with `default_scope` declaration.

--- find_a_bug.rb
+++ find_a_bug.rb
@@ -3,7 +3,7 @@ unless File.exists?('Gemfile')
   File.write('Gemfile', <<-GEMFILE)
     source 'https://rubygems.org'
     ruby '2.3.3', :engine => 'jruby', :engine_version => '9.1.15.0'
-    gem 'activerecord','5.0.6'
+    gem 'activerecord','5.1.6'
     gem 'activerecord-jdbcsqlite3-adapter','50.0'
   GEMFILE
 
@@ -17,23 +17,9 @@ config = {pool: 20, adapter: 'sqlite3', database: 'break.sqlite3'}
 ActiveRecord::Base.establish_connection(config)
 
 class ResporgAccountId < ActiveRecord::Base
-  has_many :ip_addresses
-  default_scope { where(active: true) }
-
-  def self.all_addresses
-    all.each do |resporg|
-      resporg.only_addrs
-    end
-  end
-
-  def only_addrs
-    ip_addresses.pluck(:address)
-  end
 end
 
-class IpAddress < ActiveRecord::Base
-  belongs_to :resporg_account_id
-end
+
 Thread.abort_on_exception = true
 
 
@@ -42,7 +28,7 @@ loop do
     Thread.new do
       ActiveRecord::Base.connection_pool.with_connection do
         begin
-          ResporgAccountId.all_addresses
+          ResporgAccountId.first
         rescue => exception
            puts exception.message
            puts exception.backtrace
