5c3671bfb3784d5060bb1afeb04b48ee61f12041
Viren Negi <meetme2meat@users.noreply.github.com>
2018-04-23 08:08:34 +0530

    Use AR ~> 5.0.stable

--- find_a_bug.rb
+++ find_a_bug.rb
@@ -3,7 +3,7 @@ unless File.exists?('Gemfile')
   File.write('Gemfile', <<-GEMFILE)
     source 'https://rubygems.org'
     ruby '2.3.3', :engine => 'jruby', :engine_version => '9.1.15.0'
-    gem 'activerecord','5.1.6'
+    gem 'activerecord','5.0.6'
     gem 'activerecord-jdbcsqlite3-adapter','50.0'
   GEMFILE
 
